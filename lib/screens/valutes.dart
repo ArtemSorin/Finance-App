c86741c2fdc0d79228c2b961e2a6074c4c0210b2
artemleusenko <artem20072003@outlook.com>
2022-11-19 22:20:38 +0300

    update cards

--- lib/screens/valutes.dart
+++ lib/screens/valutes.dart
@@ -21,8 +21,114 @@ class _MyAppState extends State<Valutes> {
     futureCoinsList = fetchCoinsList();
   }
 
+  List<Widget> indicators(imagesLength, currentIndex) {
+    return List<Widget>.generate(imagesLength, (index) {
+      return Container(
+        margin: EdgeInsets.all(3),
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+            color: currentIndex == index ? Colors.black : Colors.black26,
+            shape: BoxShape.circle),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    List items = [
+      Container(
+        height: height * 0.25,
+        width: width,
+        decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 91, 117, 240),
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        child: Container(
+            margin: const EdgeInsets.only(top: 90, left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    '●●●● ●●●● ●●●● 6653',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    '09/23',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    'Artem Sorin',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                ),
+              ],
+            )),
+      ),
+      Container(
+        height: height * 0.25,
+        width: width,
+        decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 91, 117, 240),
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        child: Container(
+            margin: const EdgeInsets.only(top: 90, left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    '●●●● ●●●● ●●●● 4356',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    '06/26',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    'Artem Sorin',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                ),
+              ],
+            )),
+      ),
+    ];
+
     return MaterialApp(
       title: 'Fetch Data Example',
       theme: ThemeData(
@@ -30,6 +136,14 @@ class _MyAppState extends State<Valutes> {
       ),
       home: Scaffold(
         appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () => {},
+                icon: const Icon(
+                  Icons.add_circle,
+                  color: Color.fromARGB(255, 91, 117, 240),
+                ))
+          ],
           backgroundColor: Colors.white,
           title: const Text(
             'Valutes App',
@@ -43,6 +157,20 @@ class _MyAppState extends State<Valutes> {
               if (snapshot.hasData) {
                 return ListView(
                   children: <Widget>[
+                    SizedBox(
+                      height: height * 0.35,
+                      child: Padding(
+                          padding: const EdgeInsets.all(30.0),
+                          child: PageView.builder(
+                              itemCount: 2,
+                              pageSnapping: true,
+                              itemBuilder: (context, pagePosition) {
+                                return Container(
+                                    height: height * 0.35,
+                                    margin: const EdgeInsets.all(10),
+                                    child: items[pagePosition]);
+                              })),
+                    ),
                     ListTile(
                       leading: const CircleAvatar(
                         backgroundColor: Color.fromARGB(255, 91, 117, 240),
